bb3cbd45423a3ae605cebc903fefbe00490b85c6
William <wduenas@unac.edu.co>
2016-08-29 13:00:35 -0500

    MVS

--- app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/Inicio.java
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/Inicio.java
@@ -1,12 +1,18 @@
 package com.programacion.movil.estemanp.androidmvcapplication.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 import com.programacion.movil.estemanp.androidmvcapplication.Controller.ApplicationController;
 import com.programacion.movil.estemanp.androidmvcapplication.R;
 
-public class Inicio extends AppCompatActivity {
+public class Inicio extends AppCompatActivity implements View.OnClickListener {
+
+    Button BLogin, BRegistrer;
     ApplicationController appController;
 
 
@@ -14,5 +20,26 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        BLogin=(android.widget.Button) this.findViewById(R.id.buttonIN);
+        BRegistrer=(android.widget.Button) this.findViewById(R.id.buttonUP);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonIN:
+                startActivity(new Intent(this, LoginActivity.class));
+
+                break;
+
+            case R.id.buttonUP:
+                startActivity(new Intent(this, RegisterActivity.class));
+
+                break;
+
+        }
+
     }
 }
--- app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/LoginActivity.java
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/LoginActivity.java
@@ -4,19 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.programacion.movil.estemanp.androidmvcapplication.Controller.AppController;
 import com.programacion.movil.estemanp.androidmvcapplication.Controller.ApplicationController;
 import com.programacion.movil.estemanp.androidmvcapplication.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText userName;
     EditText password;
+    Button BLogin;
+    TextView RegisterLink;
     ApplicationController appController;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +28,32 @@ public class LoginActivity extends AppCompatActivity {
 
         userName=(android.widget.EditText) this.findViewById(R.id.editUsername);
         password=(android.widget.EditText) this.findViewById(R.id.editPassword);
+        BLogin=(android.widget.Button) this.findViewById(R.id.btnLogin);
+        RegisterLink=(android.widget.TextView) this.findViewById(R.id.tvRegisterLink);
+
+        BLogin.setOnClickListener(this);
+        RegisterLink.setOnClickListener(this);
     }
 
     public void login(View view) {
         if(AppController.get_instance().isValidUser(userName.getText().toString(),password.getText().toString())) {
-            Intent intent = new Intent(this, RegisterActivity.class);
+            Intent intent = new Intent(this, Inicio.class);
             startActivity(intent);
         }else{
             Toast.makeText(this, "El usuario y contraseña no coinciden", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+
+                break;
+
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
         }
     }
 }
